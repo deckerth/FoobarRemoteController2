fb611795e30412933941ede9cc117fa20b8cec15
deckerth <deckerth@gmail.com>
2024-11-11 13:05:42 +0100

    Added formatted layout description

--- app/src/main/java/com/deckerth/thomas/foobarremotecontroller2/ui/layout/LayoutItem.kt
+++ app/src/main/java/com/deckerth/thomas/foobarremotecontroller2/ui/layout/LayoutItem.kt
@@ -1,19 +1,37 @@
 package com.deckerth.thomas.foobarremotecontroller2.ui.layout
 
-import androidx.compose.ui.text.font.FontStyle
+import com.deckerth.thomas.foobarremotecontroller2.R
+import com.deckerth.thomas.foobarremotecontroller2.ui.mainActivity
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class LayoutItem(
     val item: LayoutItems = LayoutItems.UNDEFINED,  // enable anonymous constructor
     var itemSize: ItemSize = ItemSize.UNDEFINED,
-    var italic:  Boolean = false,
+    var italic: Boolean = false,
     var bold: Boolean = false,
     var maxLines: Int = 1,
-    var alignment: TextAlignment = TextAlignment.LEFT) {
+    var alignment: TextAlignment = TextAlignment.LEFT
+) {
 
     fun verbose(): String {
         //TODO Make this nice:
-        return "item: $item, itemSize: $itemSize, italic: $italic, maxLines: $maxLines"
+        when (item) {
+            LayoutItems.ARTWORK ->
+                return mainActivity.baseContext.getString(R.string.size_property) + " ${itemSize.text}"
+
+            LayoutItems.PROGRESS ->
+                return ""
+
+            else -> {
+                val italic =
+                    if (italic) ", " + mainActivity.baseContext.getString(R.string.font_italic) else ""
+                val bold =
+                    if (bold) ", " + mainActivity.baseContext.getString(R.string.font_bold) else ""
+                val alignment =
+                    if (alignment != TextAlignment.LEFT) ", " + alignment.text else ""
+                return mainActivity.baseContext.getString(R.string.font_properties) + ": ${itemSize.text}" + italic + bold + alignment
+            }
+        }
     }
 }
--- app/src/main/java/com/deckerth/thomas/foobarremotecontroller2/ui/layout/TextAlignment.kt
+++ app/src/main/java/com/deckerth/thomas/foobarremotecontroller2/ui/layout/TextAlignment.kt
@@ -1,7 +1,10 @@
 package com.deckerth.thomas.foobarremotecontroller2.ui.layout
 
-enum class TextAlignment {
-    LEFT,
-    CENTER,
-    RIGHT
+import com.deckerth.thomas.foobarremotecontroller2.R
+import com.deckerth.thomas.foobarremotecontroller2.ui.mainActivity
+
+enum class TextAlignment(val text: String) {
+    LEFT(mainActivity.baseContext.getString(R.string.alignment_left)),
+    CENTER(mainActivity.baseContext.getString(R.string.alignment_centered)),
+    RIGHT(mainActivity.baseContext.getString(R.string.alignment_right))
 }

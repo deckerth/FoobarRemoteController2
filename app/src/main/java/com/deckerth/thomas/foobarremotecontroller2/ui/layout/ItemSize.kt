759010acc4dbc0ba5f22d27fdad6fed2fe2b7191
deckerth <deckerth@gmail.com>
2024-11-12 15:20:14 +0100

    do not allow MAX_COVER on tablets

--- app/src/main/java/com/deckerth/thomas/foobarremotecontroller2/ui/layout/ItemSize.kt
+++ app/src/main/java/com/deckerth/thomas/foobarremotecontroller2/ui/layout/ItemSize.kt
@@ -1,10 +1,15 @@
 package com.deckerth.thomas.foobarremotecontroller2.ui.layout
 
 import com.deckerth.thomas.foobarremotecontroller2.R
+import com.deckerth.thomas.foobarremotecontroller2.ui.isTablet
 import com.deckerth.thomas.foobarremotecontroller2.ui.mainActivity
 import com.deckerth.thomas.foobarremotecontroller2.ui.layout.ItemSize.entries
 
-enum class ItemSize(val text: String, val isFontSize: Boolean = true) {
+enum class ItemSize(
+    val text: String,
+    val isFontSize: Boolean = true,
+    val onTablets: Boolean = true
+) {
     UNDEFINED("UNDEFINED"),
     TITLE_LARGE(mainActivity.baseContext.getString(R.string.title_large)),
     TITLE_MEDIUM(mainActivity.baseContext.getString(R.string.title_medium)),
@@ -13,13 +18,20 @@ enum class ItemSize(val text: String, val isFontSize: Boolean = true) {
     SMALL_COVER(mainActivity.baseContext.getString(R.string.small_cover), isFontSize = false),
     MEDIUM_COVER(mainActivity.baseContext.getString(R.string.medium_cover), isFontSize = false),
     LARGE_COVER(mainActivity.baseContext.getString(R.string.large_cover), isFontSize = false),
-    MAX_COVER(mainActivity.baseContext.getString(R.string.max_cover), isFontSize = false),
+    MAX_COVER(
+        mainActivity.baseContext.getString(R.string.max_cover),
+        isFontSize = false,
+        onTablets = false
+    ),
 }
 
-fun getItemSizesForAlbums() :List<ItemSize> {
-    return entries.filter { !it.isFontSize && it != ItemSize.UNDEFINED }
+fun getItemSizesForAlbums(): List<ItemSize> {
+    return if (mainActivity.isTablet())
+        entries.filter { !it.isFontSize && it.onTablets && it != ItemSize.UNDEFINED }
+    else
+        entries.filter { !it.isFontSize && it != ItemSize.UNDEFINED }
 }
 
-fun getItemSizesForFonts() :List<ItemSize> {
-    return entries.filter { it.isFontSize && it != ItemSize.UNDEFINED}
+fun getItemSizesForFonts(): List<ItemSize> {
+    return entries.filter { it.isFontSize && it != ItemSize.UNDEFINED }
 }

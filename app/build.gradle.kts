8154a029a0d3d41673bcc8a6ddcd58405c3bf685
deckerth <deckerth@gmail.com>
2025-01-23 20:13:48 +0100

    Increase version counter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.deckerth.thomas.foobarremotecontroller2"
         minSdk = 31
         targetSdk = 34
-        versionCode = 9
+        versionCode = 10
         versionName = "2.03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

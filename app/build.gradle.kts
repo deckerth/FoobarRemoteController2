f8a6c96ca3ac3f667646d913049a7ff7398c961f
deckerth <deckerth@gmail.com>
2025-04-28 08:02:26 +0200

    New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.deckerth.thomas.foobarremotecontroller2"
         minSdk = 31
         targetSdk = 35
-        versionCode = 18
-        versionName = "2.10"
+        versionCode = 19
+        versionName = "2.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
